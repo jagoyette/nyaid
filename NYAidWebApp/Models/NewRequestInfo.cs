d994ab15fd8baff9878229793c9ed4c27b3ca596
John Goyette <John.Goyette@DentsplySirona.com>
2020-05-18 17:20:15 -0400

    Added model classes for Request API

---
+++ NYAidWebApp/Models/NewRequestInfo.cs
@@ -0,0 +1,26 @@
+﻿
+namespace NYAidWebApp.Models
+{
+    public class NewRequestInfo
+    {
+        /// <summary>
+        /// Name of the user that created the request
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Location of the request
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Phone number to contact the user that created the request
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Description of the request
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
---
+++ NYAidWebApp/Models/Request.cs
@@ -0,0 +1,30 @@
+﻿namespace NYAidWebApp.Models
+{
+    public class Request
+    {
+        /// <summary>
+        /// The unique id identifying this request
+        /// </summary>
+        public string RequestId { get; set; }
+
+        /// <summary>
+        /// Name of the user that created the request
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Location of the request
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Phone number to contact the user that created the request
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Description of the request
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
