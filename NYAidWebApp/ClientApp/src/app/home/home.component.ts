2afba0f5c85465e5c59f281c21fd1e19e92eb779
Tsvaygenbok, Leon <Leon.Tsvaygenbok@dentsplysirona.com>
2020-05-18 22:17:24 -0400

    Added Request info class to display requests. Implemented helpsomeone.

--- NYAidWebApp/ClientApp/src/app/home/home.component.ts
+++ NYAidWebApp/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public requests: RequestInfo[];
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+  }
+
   requesthelp() {
     console.log('requesthelp was called');
   }
 
   helpsomeone() {
     console.log('helpsomeone was called');
+    this.http.get<RequestInfo[]>(this.baseUrl + 'api/request').subscribe(result => {
+      this.requests = result;
+    }, error => console.error(error));
   }    
 }
---
+++ NYAidWebApp/ClientApp/src/app/models/request-info.ts
@@ -0,0 +1,7 @@
+export class RequestInfo {
+    requestId: string;
+    name: string;
+    location: string;
+    phone: string;
+    description: string;
+}
