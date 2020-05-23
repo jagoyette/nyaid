5860f0e11bbdb7e02c83e2b6a8ebfa202ee91964
John Goyette <John.Goyette@DentsplySirona.com>
2020-05-23 18:02:20 -0400

    Added UserController

---
+++ NYAidWebApp/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NYAidWebApp.Models;
+
+namespace NYAidWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly string ClaimTypeNameIdentifier = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+        private readonly string ClaimTypeEmailAddress = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+
+        [HttpGet]
+        [Authorize]
+        public UserInfo GetUser()
+        {
+            // This API call requires authentication, so the current user must
+            // already be logged in. The Easy Auth middleware will populate
+            // User claims.
+
+            // Sanity check for valid user
+            if (User == null)
+                return null;
+
+            // Get all claims
+            var claims = User.Claims.ToArray();
+
+            return new UserInfo
+            {
+                Id = claims.FirstOrDefault(c => c.Type == ClaimTypeNameIdentifier)?.Value,
+                Email = claims.FirstOrDefault(c => c.Type == ClaimTypeEmailAddress)?.Value,
+                Name = User.Identity.Name,
+                IsAuthenticated = User.Identity.IsAuthenticated
+            };
+        }
+    }
+}
---
+++ NYAidWebApp/Models/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NYAidWebApp.Models
+{
+    public class UserInfo
+    {
+        public string Id { get; set; }
+
+        public bool IsAuthenticated { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
