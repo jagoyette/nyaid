8aecbbe05807aa4caec99a411ae806a3237ab8f6
Tsvaygenbok, Leon <Leon.Tsvaygenbok@dentsplysirona.com>
2020-05-23 01:24:47 -0400

    Fixing the broken build

--- NYAidWebApp/ClientApp/src/app/updaterequest/updaterequest.component.ts
+++ NYAidWebApp/ClientApp/src/app/updaterequest/updaterequest.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NyaidWebAppApiService } from 'src/app/services/nyaid-web-app-api-service';
 import { FormBuilder, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-updaterequest',
@@ -17,7 +18,7 @@ export class UpdaterequestComponent implements OnInit {
 
   private data: RequestInfo;
   constructor(private nyaidApiService: NyaidWebAppApiService,
-    private formBuilder: FormBuilder,) { }
+    private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
     this.data = this.nyaidApiService.getData();
@@ -35,6 +36,15 @@ export class UpdaterequestComponent implements OnInit {
       description: [this.description, Validators.required]
     });
       
+  }
+
+
+  onSubmit(formData) {
+    console.log('Submitting new help request: ' + JSON.stringify(formData));
+    this.nyaidApiService.createRequest(formData).subscribe(data => {
+      console.log('New request submitted');
+      this.router.navigate(['/']);
+    });
   }
 
 }
