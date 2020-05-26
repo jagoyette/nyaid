3aff0a9de0af2a6510786339a2f5e7b131088e5f
Tsvaygenbok, Leon <Leon.Tsvaygenbok@dentsplysirona.com>
2020-05-25 21:39:17 -0400

    Fixing the broken build

--- NYAidWebApp/ClientApp/src/app/request/request.component.ts
+++ NYAidWebApp/ClientApp/src/app/request/request.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NyaidWebAppApiService } from 'src/app/services/nyaid-web-app-api-service';
+import { RequestInfo } from '../models/request-info';
 
 @Component({
   selector: 'app-request',
@@ -10,15 +11,17 @@ export class RequestComponent implements OnInit {
   public request: RequestInfo;
   private requestId: string
 
-  constructor(private nyaidApiService: NyaidWebAppApiService) { }
+  constructor(private nyaidApiService: NyaidWebAppApiService) {
+  }
 
   ngOnInit() {
     let request = this.nyaidApiService.getData();
     this.requestId = request.requestId;
     this.nyaidApiService.getRequest(this.requestId).subscribe(data => {
-      this.request = data;
+      request = data;
+      this.request = request;
       console.log('Found ' + this.request + ' request');
-    });
+    });    
   }
 
 }
--- NYAidWebApp/ClientApp/src/app/services/nyaid-web-app-api-service.ts
+++ NYAidWebApp/ClientApp/src/app/services/nyaid-web-app-api-service.ts
@@ -71,17 +71,16 @@ export class NyaidWebAppApiService {
   }
 
   /**
-   * Get Request information of a given requestId
+   * Retrieve summary of a request
    *
    * @param requestId - The ID of the request
    *
-   * @return An `Observable` of `RequestInfo` object.
-   *
+   *  @return An `Observable` of `RequestInfo` object
    */
   getRequest(requestId: string): Observable<RequestInfo> {
     const url = this.baseUrl + 'api/request' + '/' + requestId;
-    return this.http.get<RequestInfo>(url);  
-  }
+    return this.http.get<RequestInfo>(url);
+  }  
 
   /**
    * Delete Request for a given requestId
