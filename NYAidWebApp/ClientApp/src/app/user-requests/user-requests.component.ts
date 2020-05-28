6786daef1fa3ed51d80bd1c5841adb2bf7a14190
Tsvaygenbok, Leon <Leon.Tsvaygenbok@dentsplysirona.com>
2020-05-28 15:38:31 -0400

    Updated component to the mockup. Added onUpdateRequest and implemented ngOnInit

--- NYAidWebApp/ClientApp/src/app/user-requests/user-requests.component.ts
+++ NYAidWebApp/ClientApp/src/app/user-requests/user-requests.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NyaidWebAppApiService } from 'src/app/services/nyaid-web-app-api-service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-requests',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-requests.component.css']
 })
 export class UserRequestsComponent implements OnInit {
+  public requests: RequestInfo[];
 
-  constructor() { }
+  constructor(private nyaidApiService: NyaidWebAppApiService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.nyaidApiService.getAllRequests().subscribe(data => {
+      this.requests = data;
+      console.log('Found ' + this.requests.length + ' requests');
+    });
   }
 
+  onUpdateRequest(request: RequestInfo): void {
+    this.nyaidApiService.setData(request);
+    console.log('onUpdateRequest called');
+    this.goToPage('request/update');
+  }
+  
+  goToPage(pageName: string) {
+    this.router.navigate([`${pageName}`]);
+  }
 }
