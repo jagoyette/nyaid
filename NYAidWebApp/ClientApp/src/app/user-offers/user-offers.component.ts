ba29dbe7a6a2169cb08beb883f79827bcc65f34b
Tsvaygenbok, Leon <Leon.Tsvaygenbok@dentsplysirona.com>
2020-06-05 08:08:26 -0400

    Implemented the initial code and display ngInit().

--- NYAidWebApp/ClientApp/src/app/user-offers/user-offers.component.ts
+++ NYAidWebApp/ClientApp/src/app/user-offers/user-offers.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NyaidWebAppApiService } from '../services/nyaid-web-app-api-service';
+import { RequestInfo } from '../models/request-info';
+import { NyaidUserService } from '../services/nyaid-user.service';
+
 @Component({
   selector: 'app-user-offers',
   templateUrl: './user-offers.component.html',
   styleUrls: ['./user-offers.component.css']
 })
 export class UserOffersComponent implements OnInit {
+  private requests: RequestInfo[];
 
-  constructor() { }
+  constructor(private nyaidApiService: NyaidWebAppApiService,
+    private userService: NyaidUserService) { }
 
   ngOnInit() {
+    // Get all requests created by user first
+    if (this.userService.currentUser) {
+      this.nyaidApiService.getRequestsCreatedByUser(this.userService.currentUser.uid)
+        .subscribe(data => {
+          this.requests = data;
+          console.log('Found ' + this.requests.length + ' requests');
+      });
+    }
   }
-
 }
