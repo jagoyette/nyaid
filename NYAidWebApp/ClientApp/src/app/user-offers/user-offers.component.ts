4f83b5da3dd18eb894bf2575c604960931351c75
Tsvaygenbok, Leon <Leon.Tsvaygenbok@dentsplysirona.com>
2020-06-10 14:35:56 -0400

    Added code to handle a list of offers

--- NYAidWebApp/ClientApp/src/app/user-offers/user-offers.component.ts
+++ NYAidWebApp/ClientApp/src/app/user-offers/user-offers.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { NyaidWebAppApiService } from '../services/nyaid-web-app-api-service';
 import { RequestInfo } from '../models/request-info';
 import { NyaidUserService } from '../services/nyaid-user.service';
+import { OfferInfo } from '../models/offer-info';
 
 @Component({
   selector: 'app-user-offers',
@@ -11,13 +13,33 @@ import { NyaidUserService } from '../services/nyaid-user.service';
 })
 export class UserOffersComponent implements OnInit {
   public requests: RequestInfo[];
+  public offers: OfferInfo[];
+  public request: RequestInfo;
+  public offer: OfferInfo;
 
   constructor(private nyaidApiService: NyaidWebAppApiService,
-    private userService: NyaidUserService) { }
+    private userService: NyaidUserService,
+    private router: Router) { }
 
   ngOnInit() {
-    // Get all requests created by user first
+    // Get all offers created by user first
     if (this.userService.currentUser) {
+        this.nyaidApiService.getOffersCreatedByUser(this.userService.currentUser.uid, 'true')
+        .subscribe(data => {
+          this.offers = data;
+          console.log('Found ' + this.offers.length + ' requestsToUser');
+
+          this.offers.forEach(offer => {
+            console.log(offer);
+            this.nyaidApiService.getRequest(offer.requestId).subscribe(data => {
+              this.request = data;
+              this.offer = offer;
+            });
+          });
+      });
+    } else {
+      console.log('Unable to retrieve current user');
+      this.router.navigate(['login']);
     }
   }
 }
