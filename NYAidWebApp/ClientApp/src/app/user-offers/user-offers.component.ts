7e20e4ac373a396b25fe9382518dcc3806eb42e2
Tsvaygenbok, Leon <Leon.Tsvaygenbok@dentsplysirona.com>
2020-06-10 12:58:28 -0400

    Moved accept/reject functionality from user-offers to user-requests

--- NYAidWebApp/ClientApp/src/app/user-offers/user-offers.component.ts
+++ NYAidWebApp/ClientApp/src/app/user-offers/user-offers.component.ts
@@ -3,8 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { NyaidWebAppApiService } from '../services/nyaid-web-app-api-service';
 import { RequestInfo } from '../models/request-info';
 import { NyaidUserService } from '../services/nyaid-user.service';
-import { OfferInfo } from '../models/offer-info';
-import { AcceptRejectOfferInfo } from '../models/acceptrejectOffer-info';
 
 @Component({
   selector: 'app-user-offers',
@@ -13,10 +11,6 @@ import { AcceptRejectOfferInfo } from '../models/acceptrejectOffer-info';
 })
 export class UserOffersComponent implements OnInit {
   public requests: RequestInfo[];
-  public request: RequestInfo;
-  public offers: OfferInfo[];
-  public offer: OfferInfo;
-  public acceptRejectOffer: AcceptRejectOfferInfo = new AcceptRejectOfferInfo();
 
   constructor(private nyaidApiService: NyaidWebAppApiService,
     private userService: NyaidUserService) { }
@@ -24,35 +18,6 @@ export class UserOffersComponent implements OnInit {
   ngOnInit() {
     // Get all requests created by user first
     if (this.userService.currentUser) {
-      this.nyaidApiService.getOffersCreatedByUser(this.userService.currentUser.uid, 'true')
-        .subscribe(data => {
-          this.offers = data;
-          console.log('Found ' + this.offers.length + ' requestsToUser');
-
-          this.offers.forEach(offer => {
-            console.log(offer);
-            this.nyaidApiService.getRequest(offer.requestId).subscribe(data => {
-              this.request = data;
-              this.offer = offer;
-            });
-          });
-      });
     }
   }
-
-  onAcceptOffer(request: RequestInfo): void {
-    console.log('onAcceptOffer called');
-    this.acceptRejectOffer.isAccepted = true;
-    this.nyaidApiService.acceptOffer(request.requestId, this.offer.offerId, this.acceptRejectOffer).subscribe(data => {
-      this.offer = data;
-    });
-  }
-
-  onRejectOffer(request: RequestInfo): void {
-    console.log('onRejectOffer called');
-    this.acceptRejectOffer.isAccepted = false;
-    this.nyaidApiService.acceptOffer(request.requestId, this.offer.offerId, this.acceptRejectOffer).subscribe(data => {
-      this.offer = data;
-    });  }
-
 }
--- NYAidWebApp/ClientApp/src/app/user-requests/user-requests.component.ts
+++ NYAidWebApp/ClientApp/src/app/user-requests/user-requests.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { NyaidWebAppApiService } from '../services/nyaid-web-app-api-service';
 import { RequestInfo } from '../models/request-info';
 import { NyaidUserService } from '../services/nyaid-user.service';
+import { OfferInfo } from '../models/offer-info';
+import { AcceptRejectOfferInfo } from '../models/acceptrejectOffer-info';
 
 @Component({
   selector: 'app-user-requests',
@@ -12,6 +14,10 @@ import { NyaidUserService } from '../services/nyaid-user.service';
 })
 export class UserRequestsComponent implements OnInit {
   public requests: RequestInfo[];
+  public request: RequestInfo;
+  public offers: OfferInfo[];
+  public offer: OfferInfo;
+  public acceptRejectOffer: AcceptRejectOfferInfo = new AcceptRejectOfferInfo();
 
   constructor(private nyaidApiService: NyaidWebAppApiService,
     private userService: NyaidUserService,
@@ -24,6 +30,20 @@ export class UserRequestsComponent implements OnInit {
           this.requests = data;
           console.log('Found ' + this.requests.length + ' requests');
         });
+
+        this.nyaidApiService.getOffersCreatedByUser(this.userService.currentUser.uid, 'true')
+        .subscribe(data => {
+          this.offers = data;
+          console.log('Found ' + this.offers.length + ' requestsToUser');
+
+          this.offers.forEach(offer => {
+            console.log(offer);
+            this.nyaidApiService.getRequest(offer.requestId).subscribe(data => {
+              this.request = data;
+              this.offer = offer;
+            });
+          });
+      });
     } else {
       console.log('Unable to retrieve current user');
       this.router.navigate(['login']);
@@ -32,5 +52,21 @@ export class UserRequestsComponent implements OnInit {
 
   onUpdateRequest(request: RequestInfo): void {
     this.router.navigate(['requests', request.requestId, 'update']);
+  }
+
+  onAcceptOffer(request: RequestInfo): void {
+    console.log('onAcceptOffer called');
+    this.acceptRejectOffer.isAccepted = true;
+    this.nyaidApiService.acceptOffer(request.requestId, this.offer.offerId, this.acceptRejectOffer).subscribe(data => {
+      this.offer = data;
+    });
+  }
+
+  onRejectOffer(request: RequestInfo): void {
+    console.log('onRejectOffer called');
+    this.acceptRejectOffer.isAccepted = false;
+    this.nyaidApiService.acceptOffer(request.requestId, this.offer.offerId, this.acceptRejectOffer).subscribe(data => {
+      this.offer = data;
+    });  
   }
 }
