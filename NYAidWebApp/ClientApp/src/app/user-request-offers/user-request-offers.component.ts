159f7ef90aea83b20f1ef47978dc2d09d77bc566
Tsvaygenbok, Leon <Leon.Tsvaygenbok@dentsplysirona.com>
2020-06-11 22:08:01 -0400

    Implemented card body. Implemented ngOnInit() to show a selected request.

--- NYAidWebApp/ClientApp/src/app/user-request-offers/user-request-offers.component.ts
+++ NYAidWebApp/ClientApp/src/app/user-request-offers/user-request-offers.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { NyaidWebAppApiService } from '../services/nyaid-web-app-api-service';
+import { RequestInfo } from '../models/request-info';
 
 @Component({
   selector: 'app-user-request-offers',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-request-offers.component.css']
 })
 export class UserRequestOffersComponent implements OnInit {
+  public request: RequestInfo = new RequestInfo();
 
-  constructor() { }
+  constructor(private nyaidApiService: NyaidWebAppApiService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // The desired route Id should be extracted from query params
+    // and used to populate this request
+    const requestId = this.route.snapshot.paramMap.get('Id');
+    this.nyaidApiService.getRequest(requestId).subscribe(data => {
+      this.request = data;
+    });
   }
 
 }
