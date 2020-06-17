9c4fff94eb93fe3ff4a7d05a65f36ca6767dadf5
John Goyette <John.Goyette@DentsplySirona.com>
2020-06-17 00:16:38 -0400

    Added displayState and displayPhone attributes

--- NYAidWebApp/ClientApp/src/app/components/request-card/request-card.component.ts
+++ NYAidWebApp/ClientApp/src/app/components/request-card/request-card.component.ts
@@ -8,10 +8,29 @@ import { RequestInfo } from '../../models/request-info';
 })
 export class RequestCardComponent implements OnInit {
   @Input() request: RequestInfo;
+  @Input() displayState: boolean;
+  @Input() displayPhone: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public getRequestStateString(request: RequestInfo): string {
+    let stateString = '';
+
+    switch (request.state) {
+      case 'open':
+        stateString = 'Open';
+        break;
+      case 'inProcess':
+        stateString = 'Started';
+        break;
+      case 'closed':
+        stateString = 'Finished';
+        break;
+    }
+
+    return stateString;
+  }
 }
