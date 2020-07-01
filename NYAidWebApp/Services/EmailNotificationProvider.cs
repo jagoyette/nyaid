b65bd9a45ff5e2a244ef1123364f0d53695070b6
John Goyette <John.Goyette@DentsplySirona.com>
2020-06-30 23:09:40 -0400

    Refactored Email provider to re-use card HTML

--- NYAidWebApp/Services/EmailNotificationProvider.cs
+++ NYAidWebApp/Services/EmailNotificationProvider.cs
@@ -83,29 +83,16 @@ namespace NYAidWebApp.Services
                     From = new EmailAddress(FROM_EMAIL_ADDRESS, FROM_EMAIL_NAME),
                     Subject = "Someone offered to help you!",
                     HtmlContent = $@"
-<html>
-    <body>
-        <p>Someone has offered to help with your request that you submitted on Friendly!
-        </p>
-        <div style=""box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"">
-            <div style=""padding: .75rem 1.25rem;margin-bottom: 0;background-color: rgba(0,0,0,.03);border-bottom: 1px solid rgba(0,0,0,.125);"">
-                <p>
-                    <span style=""margin-bottom: .75rem;font-size: 1.25rem;font-weight: 400;"">{request.Location}</span>
-                    <br>
-                    <span style=""font-size: 80%;font-weight: 300;"">{request.Created:d}</span>
-                 </p>
-            </div>
-            <div style=""padding: 2px 16px;"">
-                <p>{request.Description}</p>
-             </div>
-        </div>
-        <p>The description of the offer is:</p>
-        <blockquote>{offer.Description}</blockquote>
-        <p>Please <a href=""{WEBSITE_URL}/request/{request.RequestId}/offers"">respond</a> to this offer on <a href=""{WEBSITE_URL}"">Friendly</a>.
-        </p>
-    </body>
-</html>
-"
+                    <html>
+                        <body>
+                            <p>Someone has offered to help with your request that you submitted on Friendly!</p>
+                            {CreateRequestCardHtml(request)}
+                            <p>The description of the offer is:</p>
+                            <blockquote>{offer.Description}</blockquote>
+                            <p>Please <a href=""{WEBSITE_URL}/request/{request.RequestId}/offers"">respond</a> to this offer on <a href=""{WEBSITE_URL}"">Friendly</a>.</p>
+                        </body>
+                    </html>
+                    "
                 };
                 msg.AddTo(new EmailAddress(user.Email, user.Name));
                 var response = await client.SendEmailAsync(msg);
@@ -116,6 +103,29 @@ namespace NYAidWebApp.Services
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// Returns an HTML formatted string representing the Request
+        /// </summary>
+        /// <param name="request">The request to format</param>
+        /// <returns>a string formatted as HTML</returns>
+        private string CreateRequestCardHtml(Request request)
+        {
+            return $@"
+                <div style=""box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);"">
+                    <div style=""padding: .75rem 1.25rem;margin-bottom: 0;background-color: rgba(0,0,0,.03);border-bottom: 1px solid rgba(0,0,0,.125);"">
+                        <p>
+                            <span style=""margin-bottom: .75rem;font-size: 1.25rem;font-weight: 400;"">{request.Location}</span>
+                            <br>
+                            <span style=""font-size: 80%;font-weight: 300;"">{request.Created:d}</span>
+                         </p>
+                    </div>
+                    <div style=""padding: 2px 16px;"">
+                        <p>{request.Description}</p>
+                     </div>
+                </div>
+            ";
         }
     }
 }
