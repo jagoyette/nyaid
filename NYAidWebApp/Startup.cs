c5af26cfecc0154c452aa81e1dcc38aba33e4a01
John Goyette <John.Goyette@DentsplySirona.com>
2020-07-04 22:26:06 -0400

    Task #210: Apply database migrations at startup

--- NYAidWebApp/Startup.cs
+++ NYAidWebApp/Startup.cs
@@ -77,6 +77,9 @@ namespace NYAidWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApiDataContext ctx)
         {
+            // Apply database migrations
+            ctx.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
