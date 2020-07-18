9e701bef61e25183c0cf51141df336019b47f1f5
John Goyette <John.Goyette@dentsplysirona.com>
2020-07-18 19:20:45 -0400

    Task #208: Added PUT Api to allow updating user preferences

--- NYAidWebApp/Controllers/UserController.cs
+++ NYAidWebApp/Controllers/UserController.cs
@@ -47,12 +47,50 @@ namespace NYAidWebApp.Controllers
             if (dbUser == null)
             {
                 _log.LogInformation($"Adding user {user.Name} to database.");
-                await _context.Users.AddAsync(user);
+                var result = await _context.Users.AddAsync(user);
                 await _context.SaveChangesAsync();
                 _log.LogInformation($"Finished updating database");
+
+                // Assign new user to return
+                dbUser = result.Entity;
             }
 
-            return user;
+            return dbUser;
+        }
+
+        [HttpPut]
+        [Authorize]
+        public async Task<UserInfo> UpdateUser(UserInfo userInfo)
+        {
+            _log.LogInformation($"Updating current user info");
+
+            // This API call requires authentication, so the current user must
+            // already be logged in. The Easy Auth middleware will populate
+            // User claims.
+
+            // Create a UserInfo from the current user claims
+            var user = _userService.CreateUserInfoFromClaims(User);
+
+            // Sanity check for valid user
+            if (user == null)
+                return null;
+
+            // get the user from our Db
+            var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Uid == user.Uid);
+            if (dbUser == null)
+            {
+                _log.LogError($"User {user.Uid} has no entry in the database");
+                return null;
+            }
+
+            _log.LogInformation($"Updating user preferences for user {userInfo.Uid}");
+
+            dbUser.EmailNotificationsEnabled = userInfo.EmailNotificationsEnabled;
+            _context.Users.Update(dbUser);
+            await _context.SaveChangesAsync();
+
+            _log.LogInformation($"Finished updating user preferences.");
+            return dbUser;
         }
 
         [HttpGet]
@@ -63,5 +101,6 @@ namespace NYAidWebApp.Controllers
             return await _context.Users
                 .FirstOrDefaultAsync(u => u.Uid == uid);
         }
+
     }
 }
